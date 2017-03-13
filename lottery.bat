735a7aaff699430559d850d9bda80cc9976abfac
Vocatus Gate <vocatus.gate@gmail.com>
2017-03-13 12:34:37 -0700

    Update lottery.bat

--- lottery.bat
+++ lottery.bat
@@ -1,5 +1,5 @@
 :: Loops through a comparison of %RANDOM% and any time %RANDOM% is less than 3, adds a point to the "tick counter"
-:: At 14:30 the script terminates and displays how many tick hits there were (how many times %RANDOM% was less than 3)
+:: At *2:30 the script terminates and displays how many tick hits there were (how many times %RANDOM% was less than 3)
 
 @echo off
 set LAUNCH_TIME=%TIME%
@@ -10,12 +10,12 @@ if %RANDOM% LSS 3 (
 	color 0a
 	set /a "WINS=%WINS%+1"
 	echo  %TIME%   TICK HIT ^(hits: !WINS!^)
-	if %time:~1,3% equ 4:3 goto :done
+	if %time:~1,3% equ 2:3 goto :done
 	)
 goto :s
 
 
 :done
-echo  %TIME% ! Time limit reached
+echo  %TIME% ^! Time limit reached
 echo  %TIME%   TOTAL HITS: %WINS%
 pause
