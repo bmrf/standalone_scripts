2f11c57847268e789ce06b579881c2c8c541d985
Vocatus Gate <vocatus.gate@gmail.com>
2019-12-13 09:43:38 -0700

    Update Linux one-liners.sh

--- Linux one-liners.sh
+++ Linux one-liners.sh
@@ -1,5 +1,8 @@
-# SCP a file from one system to another
-scp username@destination_host:/path/to/file /path/to/destination
+# Matrix text scrolling effect in the terminal
+echo -e "\e[1;40m" ; clear ; while :; do echo $LINES $COLUMNS $(( $RANDOM % $COLUMNS)) $(( $RANDOM % 72 )) ;sleep 0.05; done|awk '{ letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()"; c=$4; letter=substr(letters,c,1);a[$3]=0;for (x in a) {o=a[x];a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,letter; printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;if (a[x] >= $1) { a[x]=0; } }}'
+
+# Calculate cost of lines of code in a directory
+sloccount ./
 
 # List all services/daemons that run at startup (run level 3)
 /sbin/chkconfig --list |grep '3:on'
