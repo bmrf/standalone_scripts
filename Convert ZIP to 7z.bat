03a602498032a6f29225637bf0adbfa25227f783
Vocatus Gate <vocatus.gate@gmail.com>
2020-11-15 15:31:24 -0700

    Update Convert ZIP to 7z.bat

--- Convert ZIP to 7z.bat
+++ Convert ZIP to 7z.bat
@@ -7,3 +7,6 @@
 pushd "%~dp0" 2>NUL
 
 for %%F in (*.zip) do ( "C:\Program Files\7-Zip\7z.exe" x -y -o"%%F_tmp" "%%F" * & pushd %%F_tmp & "C:\Program Files\7-Zip\7z.exe" a -y -r -t7z ..\"%%~nF".7z * & popd & rmdir /s /q "%%F_tmp" )
+
+:: Cleanup
+del /f /q *.zip
